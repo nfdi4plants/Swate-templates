ca1655fdddc45ae7ee543c49d61517e78a161431
omaus <omaus@bio.uni-kl.de>
2021-08-17 14:58:18 +0200

    Update gitignore to delete temp files

--- output/templates/Plant growth.fsx
+++
@@ -1,5 +0,0 @@
-(**
-// can't yet format YamlFrontmatter (["title: Plant growth"; "category: Templates"; "categoryindex: 1"; "index: 1"], Some { StartLine = 2 StartColumn = 0 EndLine = 6 EndColumn = 8 }) to pynb markdown
-
-test
-*)
